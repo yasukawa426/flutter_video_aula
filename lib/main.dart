027af56785a66b4426fec3b5d3f786b2452570a6
Lucas Luquesi Yasukawa <yasukawa.luquesi.lucas@gmail.com>
2022-05-27 17:20:53 -0300

    oi

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,13 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(Container());
+  runApp(Container(
+    child: Center(
+      child: Text(
+      'Oi',
+      textDirection: TextDirection.ltr,
+      style: TextStyle(color: Colors.white, fontSize:50.0),
+    ),
+    ),
+  ));
 }
