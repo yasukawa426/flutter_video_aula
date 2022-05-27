e3e6f003f268f2f88935d4cf746fca5387914ed9
Lucas Luquesi Yasukawa <yasukawa.luquesi.lucas@gmail.com>
2022-05-27 17:38:41 -0300

    Experimentso de widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,27 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(Container(
-    child: Center(
-      child: Text(
-      'Oi',
-      textDirection: TextDirection.ltr,
-      style: TextStyle(color: Colors.white, fontSize:50.0),
-    ),
-    ),
-  ));
+  runApp(const AppWidget(
+    title:"Teste"
+  )); // no começo do app roda a classe AppWidget
+}
+
+class AppWidget extends StatelessWidget {
+
+  final String title;
+
+  const AppWidget({super.key, required this.title}); //contrutor q incializa tds as variaveis
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text(
+          title,
+          textDirection: TextDirection.ltr,
+          style: TextStyle(color: Colors.white, fontSize: 50.0),
+        ),
+      ),
+    );
+  }
 }
