b0ec2b2a69836aec1670504053df94f24334ec03
Lucas Luquesi Yasukawa <yasukawa.luquesi.lucas@gmail.com>
2022-05-29 19:03:35 -0300

    Codigo refatorado para cada widget ter sua propria classe

--- lib/main.dart
+++ lib/main.dart
@@ -1,53 +1,10 @@
 import 'package:flutter/material.dart';
 
+import 'app_widget.dart';
+
 void main() {
   runApp(const AppWidget(
       title: "Teste")); // no começo do app roda a classe AppWidget
 }
 
-class AppWidget extends StatelessWidget {
-  final String title;
 
-  const AppWidget(
-      {super.key,
-      required this.title}); //contrutor q incializa tds as variaveis
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        //esse materialApp é um widget q da a cara de um app pro app. Basicamente um tema de app da google
-        theme: ThemeData(
-            primarySwatch:
-                Colors.blue //faz ele trabalhar com essa palheta de cor
-            ),
-        home: HomePage());
-  }
-}
-
-//stateful widget que chama o state dele msm. O HomePage vai ser chamado no home do material app
-class HomePage extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    return HomePageState();
-  }
-}
-
-//state de homepage
-class HomePageState extends State<HomePage> {
-  int counter = 0;
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: GestureDetector(
-          child: Text('Contador: $counter'),
-          onTap: () {
-            setState(() { //o setstate é necessario para o estado atualizar. Se n, n veriamos nenhuma atualização na tela
-            counter++;
-            });
-            print('clicado $counter vezes');
-            
-          }),
-    );
-  }
-}
