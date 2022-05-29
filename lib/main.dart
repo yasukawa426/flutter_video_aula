dca88b814e72c6630555dc6bfaa21173be438760
Lucas Luquesi Yasukawa <yasukawa.luquesi.lucas@gmail.com>
2022-05-29 18:20:56 -0300

    StatefulWidget criado

--- lib/main.dart
+++ lib/main.dart
@@ -2,26 +2,40 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(const AppWidget(
-    title:"Teste"
-  )); // no começo do app roda a classe AppWidget
+      title: "Teste")); // no começo do app roda a classe AppWidget
 }
 
 class AppWidget extends StatelessWidget {
-
   final String title;
 
-  const AppWidget({super.key, required this.title}); //contrutor q incializa tds as variaveis
+  const AppWidget(
+      {super.key,
+      required this.title}); //contrutor q incializa tds as variaveis
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text(
-          title,
-          textDirection: TextDirection.ltr,
-          style: TextStyle(color: Colors.white, fontSize: 50.0),
-        ),
-      ),
-    );
+    return MaterialApp(
+        //esse materialApp é um widget q da a cara de um app pro app. Basicamente um tema de app da google
+        theme: ThemeData(
+            primarySwatch:
+                Colors.blue //faz ele trabalhar com essa palheta de cor
+            ),
+        home: HomePage());
+  }
+}
+
+//stateful widget que chama o state dele msm. O HomePage vai ser chamado no home do material app
+class HomePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return HomePageState();
+  }
+}
+
+//state de homepage
+class HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text('Oie'));
   }
 }
