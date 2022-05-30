3728e4290a523c939e3dd63da322fad7564db915
Lucas Luquesi Yasukawa <yasukawa.luquesi.lucas@gmail.com>
2022-05-29 22:13:45 -0300

    o login agr leva para a homePage

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
+import 'home_page.dart';
+
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
 
@@ -16,11 +18,12 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SingleChildScrollView(//da um scroll pra cada filho dele
-          child: SizedBox(
-              width: MediaQuery.of(context).size.width,
-              height: MediaQuery.of(context).size.height,
-              child: Padding(
+        body: SingleChildScrollView(
+      //da um scroll pra cada filho dele
+      child: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: Padding(
           padding: const EdgeInsets.all(8.0),
           child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
             TextField(
@@ -50,17 +53,19 @@ class _LoginPageState extends State<LoginPage> {
             ElevatedButton(
               onPressed: () {
                 if (email == "email@email.com" && password == "password") {
-                  print("certo");
-                } 
-                else {
+                  //Navigator.of(context).push(//coloca a proxima tela em cima da atual, podendo voltar caso quiser
+                  Navigator.of(context).pushReplacement(//descarta a ultima tela, substituindo ela
+                    MaterialPageRoute(builder: (context) => HomePage())
+                  );
+                } else {
                   print("errado");
                 }
               },
               child: const Text("Entrar"),
             )
           ]),
-              ),
-            ),
-        ));
+        ),
+      ),
+    ));
   }
 }
