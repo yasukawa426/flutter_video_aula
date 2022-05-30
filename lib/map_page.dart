1c0a68e5c2e56ce4b408902c956dfdd3839d056c
Lucas Luquesi Yasukawa <yasukawa.luquesi.lucas@gmail.com>
2022-05-29 22:49:58 -0300

    Tela mapa criada

---
+++ lib/map_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+import 'home_page.dart';
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Mapa"),
+      ),
+      bottomNavigationBar: CustomBottomBar(index: 1),
+    );
+  }
+}
