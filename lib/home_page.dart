5d8e22190c206ec459d34a539baa15da5cd83302
Lucas Luquesi Yasukawa <yasukawa.luquesi.lucas@gmail.com>
2022-05-29 21:15:19 -0300

    Switch de tema transformado em switch e adicionado no appBar

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -17,14 +17,11 @@ class HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Home Page')),
-      body: Center(
-          child: Switch(
-        value: AppController.instance.isDarkTheme,
-        onChanged: (value) {
-          AppController.instance.changeTheme();
-        },
-      )),
+      appBar: AppBar(
+        title: const Text('Home Page'),
+        actions: [CustomSwitch()],
+      ),
+      body: CustomSwitch(),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
         onPressed: () {
@@ -49,6 +46,18 @@ class HomePageState extends State<HomePage> {
           ),
         ],
       ),
+    );
+  }
+}
+
+class CustomSwitch extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Switch(
+      value: AppController.instance.isDarkTheme,
+      onChanged: (value) {
+        AppController.instance.changeTheme();
+      },
     );
   }
 }
